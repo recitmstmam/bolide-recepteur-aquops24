5c27750b6bd8df0aef3a6888b5c69a887f4dd33e
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2024-02-08 09:53:31 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("Go")
+    basic.pause(100)
+    basic.showIcon(IconNames.No)
+    basic.pause(1000)
+    control.reset()
+})
+radio.setGroup(1)
+let Fentes = 0
+let distance = 0
+basic.showIcon(IconNames.Yes)
 basic.forever(function () {
 	
 })
